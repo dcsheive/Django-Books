71032c830f063199fef125492ad6db6f7b33967b
dcsheive <dcsheive@gmail.com>
2020-01-27 19:06:29 -0500

    update.sh

--- update.sh
+++ update.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 PUBLIC=${1?Error: No Public IP given}
 
-cd ..
 git pull 
+cd ..
 path_to_settings="Django-Books/Login/settings.py"
 PUBLIC_="['${PUBLIC}']" 
 False=False
@@ -11,9 +11,9 @@ sed -i "s/\("DEBUG" *= *\).*/\1$False/" $path_to_settings
 sed -i "s/\("ALLOWED_HOSTS" *= *\).*/\1$PUBLIC_/" $path_to_settings
 echo "STATIC_ROOT = os.path.join(BASE_DIR, 'static/')" >> $path_to_settings
 
-sudo source ./venv/bin/activate
-sudo python3 Django-Books/manage.py migrate
-sudo python3 Django-Books/manage.py collectstatic --noinput
+source ./venv/bin/activate
+python3 Django-Books/manage.py migrate
+python3 Django-Books/manage.py collectstatic --noinput --clear
 sudo systemctl daemon-reload
 sudo systemctl enable nginx
 sudo service nginx restart
