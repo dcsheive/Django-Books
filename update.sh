fb86f3052946ff3014f009ceff20c2a8f5e16dfd
dcsheive <dcsheive@gmail.com>
2020-01-27 19:49:46 -0500

    Change

--- update.sh
+++ update.sh
@@ -12,7 +12,6 @@ sed -i "s/\("ALLOWED_HOSTS" *= *\).*/\1$PUBLIC_/" $path_to_settings
 echo "STATIC_ROOT = os.path.join(BASE_DIR, 'static/')" >> $path_to_settings
 
 source ./venv/bin/activate
-python3 Django-Books/manage.py migrate
 python3 Django-Books/manage.py collectstatic --noinput --clear
 sudo systemctl daemon-reload
 sudo systemctl enable nginx
